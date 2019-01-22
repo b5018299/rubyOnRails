cc156f852f0f0a528368d31dc939d2f96eec50bb
jbloggs <jbloggs@advsysarchs2018.localdomain>
2019-01-22 00:44:56 +0000

    Created Rails App and Configured Bootstrap

---
+++ bookstore_app/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'static_pages/Home'
+  get 'static_pages/About'
+  get 'static_pages/Contact'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
